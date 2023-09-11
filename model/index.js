4e8629ef2687705e73393f6d1c7d351c768ee2c3
Dharmendra <mandaldk11@gmail.com>
2023-09-11 11:42:46 +0530

    insert jwt auth and password hashing via bcrypt also host in mongodb cloud

--- model/index.js
+++ model/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-
+const bcrypt =require('bcrypt')
 // DB-connection
 
 main().catch(err => console.log(err));
 
 async function main() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/mongoose_test');
+    await mongoose.connect('mongodb+srv://mandaldk11:apunKaRestApi@cluster1.vkfe6ac.mongodb.net/?retryWrites=true&w=majority');
     console.log('db connected...');
   } catch (err) {
     console.error('Error connecting to the database:', err);
@@ -27,8 +27,25 @@ const studentSchema = new mongoose.Schema({
     course: {
       type: String,
       required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    confirm_password: {
+      type: String,
+      required: true
     }
   });
+  // password hashing --
+  studentSchema.pre('save',async function(next){
+    if (this.isModified('password' || 'confirm_password')) {
+      this.password = await bcrypt.hash(this.password,12);
+      this.confirm_password = await bcrypt.hash(this.confirm_password,12);
+    }
+    next()
+   
+  })
 
   // Model
 const Student = mongoose.model('Student', studentSchema);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = 3000;
-
+const jwt = require('jsonwebtoken')
 const router = require('./routes/router');
 
 
@@ -11,6 +11,17 @@ app.use(express.json());
 
 // routers-
 app.use(router)
+
+// jwt tokens --
+const creatTokens =async()=>{
+  const token = await jwt.sign({_id:"64feac14eee0b1745d6bc0ac"},'helloworldmynameisdharmendramandalfromrakseha',{expiresIn:"2 minutes"})
+  console.log(token)
+
+  const verifiedUser = await jwt.verify(token,'helloworldmynameisdharmendramandalfromrakseha')
+  console.log(verifiedUser)
+
+}
+creatTokens()
 
 
 app.listen(PORT, () => {
